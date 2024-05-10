7b2fc3b35b8029a85d03da39a392b1718831e611
esheshka <as.yushinskiy@gmail.com>
2024-05-11 00:17:53 +0300

    new file:   dependency_graph.png 	modified:   pom.xml 	modified:   src/test/java/com/beginsecure/domain/ProgTest.java 	modified:   target/test-classes/com/beginsecure/domain/ProgTest.class

--- src/test/java/com/beginsecure/domain/ProgTest.java
+++ src/test/java/com/beginsecure/domain/ProgTest.java
@@ -9,6 +9,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import java.io.File;
+
+import javax.swing.*;
+import java.awt.*;
+
 class ProgTest {
 
     @Test
@@ -39,10 +53,10 @@ class ProgTest {
     void testPerformance() throws IOException {
         Random random = new Random();
 
-        int limit = 30;
+        int limit = 13;
         int[] sizes = new int[limit];
         for (int i = 0; i < limit; ++i) {
-            sizes[i] = (int) Math.pow(10, 6);
+            sizes[i] = (int) Math.pow(2, i);
         }
         long[] times = new long[limit];
 
@@ -58,6 +72,48 @@ class ProgTest {
             times[i] = endTime - startTime;
 
             assertTrue(times[i] <= 20000000, "Performance test failed" + " " + times[i]);
+        }
+
+        long[] data1 = new long[limit];
+        for (long i = 0; i < limit; i++) {
+            data1[(int) i] = i;
+        }
+        XYSeries series = new XYSeries("Data");
+        for (int i = 1; i < data1.length; i++) {
+            series.add(data1[i], times[i]);
+        }
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Dependency Graph",
+                "X Axis",
+                "Y Axis",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        plot.setRenderer(renderer);
+
+        JFrame frame = new JFrame("Graph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        frame.add(chartPanel, BorderLayout.CENTER);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+
+        try {
+            ChartUtils.saveChartAsPNG(new File("dependency_graph.png"), chart, 800, 600);
+            System.out.println("Изображение сохранено как dependency_graph.png");
+        } catch (IOException e) {
+            System.err.println("Ошибка при сохранении изображения: " + e.getMessage());
         }
     }
 
@@ -79,7 +135,6 @@ class ProgTest {
 
             Prog._min(numbers);
             long usedBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-
             assertTrue(usedBytes < 50000000, "Memory test failed" + " " + usedBytes);
         }
     }
